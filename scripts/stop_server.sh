35a2c907e034b6d0481525d9c78b74b3b918cc8d
renasami <drrr0502@gmail.com>
2022-02-04 16:44:12 +0900

    add bar chart

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,4 @@
 
 kill -INT $(lsof -t -i tcp:4400)
 kill -INT $(lsof -t -a -c node -i tcp:8000)
+kill -INT $(lsof -t -i tcp:3000)
--- src/components/pages/Admin/index.tsx
+++ src/components/pages/Admin/index.tsx
@@ -1,12 +1,43 @@
 import {FC} from "react"
 import BasicTemplate from "../../templates/BasicTemplate"
+import { Bar } from '@ant-design/plots';
 
 const Admin:FC = () => {
+    const data = [
+        {
+          year: '1951 年',
+          value: 38,
+        },
+        {
+          year: '1952 年',
+          value: 52,
+        },
+        {
+          year: '1956 年',
+          value: 61,
+        },
+        {
+          year: '1957 年',
+          value: 145,
+        },
+        {
+          year: '1958 年',
+          value: 48,
+        },
+      ];
+      const config = {
+        data,
+        xField: 'value',
+        yField: 'year',
+        seriesField: 'year',
+        
+      };
     return (
         <>
             <BasicTemplate>
             <h1>Admin</h1>
             <h3>ここに進捗とかのグラフ持ってくる</h3>
+            <Bar {...config} />
             </BasicTemplate>
         </>
     )
