aa5c0a195de346fd932e51950e445a7a0379e47a
renasami <drrr0502@gmail.com>
2022-03-03 00:35:56 +0900

    added

--- src/texts/configs.ts
+++ src/texts/configs.ts
@@ -63,31 +63,56 @@ export const courseList:Courses[] = [
     {
         title: "stage3 フロント",
         phase:"1",
-        index:2,
+        index:5,
         dir:"step3-f",
         lectures:[
             {
-                title: "1/2",
+                title: "1/5",
                 thumbnail:"",
                 index:0
             },
             {
-                title: "2/2",
+                title: "2/5",
                 thumbnail:"",
                 index:1
+            },
+            {
+                title: "3/5",
+                thumbnail:"",
+                index:2
+            },
+            {
+                title: "4/5",
+                thumbnail:"",
+                index:3
+            },
+            {
+                title: "5/5",
+                thumbnail:"",
+                index:4
             },
         ]
     },
     {
         title: "stage3 バックエンド",
         phase:"1",
-        index:1,
+        index:3,
         dir:"step3-b",
         lectures:[
             {
-                title: "1/1",
+                title: "1/3",
                 thumbnail:"",
                 index:0
+            },
+            {
+                title: "2/3",
+                thumbnail:"",
+                index:1
+            },
+            {
+                title: "3/3",
+                thumbnail:"",
+                index:2
             },
         ]
     },
